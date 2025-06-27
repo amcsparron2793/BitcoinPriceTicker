5f8e42acc6ed516f7ea6549b39adb4f585e914db
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2025-06-27 14:11:01 -0400

    WIP Add argparse support for configuring Ticker parameters
    Introduced a new `TickerArgparse` class to handle command-line arguments such as mode, crypto type, and base URL. Updated the main function to utilize these arguments, enabling greater flexibility in configuring the Ticker dynamically. WIP

--- CryptoPriceTickers/ticker.py
+++ CryptoPriceTickers/ticker.py
@@ -3,6 +3,27 @@ from typing import Optional
 from MultiTicker.multi_ticker import MultiTicker
 from Backend.factory import TickerFactory
 from Backend.helpers import CryptoType
+import argparse
+
+class TickerArgparse(argparse.ArgumentParser):
+    def __init__(self):
+        super().__init__()
+        self.add_argument('-m', '--mode',
+                          type=str, required=False,
+                          help='ticker mode', default='multi')
+        self.add_argument('-c', '--crypto_type',
+                          type=str, required=False,
+                          help='crypto type')
+        self.add_argument('-p', '--params',
+                          type=str, required=False,
+                          help='params')
+        self.add_argument('-u', '--base_url',
+                          type=str, required=False,
+                          help='base url')
+        self.add_argument('-uc', '--use_colorizer',
+                          action='store_true', required=False,
+                          default=True,
+                          help='enable colorizer')
 
 
 class Ticker:
@@ -52,7 +73,13 @@ class Ticker:
 
 
 if __name__ == '__main__':
+    # TODO: This is for testing basic ideas, needs a lot of further work
+    arg_parser = TickerArgparse()
+    print(arg_parser.print_help())
+    args = arg_parser.parse_args()
+
     tk_factory = TickerFactory()
     # mt = Ticker(mode='multi')
-    mt = Ticker(mode='factory', factory=tk_factory, crypto_type='bitcoin')
+    mt = Ticker(mode=args.mode, use_colorizer=args.use_colorizer)
+    #mt = Ticker(mode='factory', factory=tk_factory, crypto_type='bitcoin')
     mt.run()
